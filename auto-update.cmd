cd475999f451f05013519ee5aaff00dd75c9a2c5
loadPoint <loadPoint2003@outlook.com>
2024-06-17 03:36:16 +0800

    Auto commit at 20240617-033616

--- auto-update.cmd
+++ auto-update.cmd
@@ -1,4 +1,5 @@
 @echo off
+set http_proxy=http://127.0.0.1:7890 & set https_proxy=http://127.0.0.1:7890
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do set datetime=%%I
 set datetime=%datetime:~0,8%-%datetime:~8,6%
 git add --all
